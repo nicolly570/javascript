b49993b59df5cc9bbfb9702d1fc02b4162c44261
nicolly570 <nicollysoarescontato@gmail.com>
2024-09-26 16:55:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function solicitarDados() {
+    //Solicita o nome do usuario 
+    let name =prompt ("qual seu nome?")
+    //Solicita a idade do usuario
+    let idade = prompt("qual sua idade?")
+    //Solicite o telefone do usuario
+    let telefone = prompt("qual o seu telefone?")
+
+
+
+    //Exibe a saudação
+alert ("Ola, " + nome + "! Você tem " + idade + anos  + local + endereço+ "Qual seu telefone?" + numero  )
+//Mostra os dados no console 
+conaole=log("Nome do usuario:". nome)
+console=log("idade do usuario:")
+console=log("telefone do usuario")
+}
